69cd83c0e474270901fc46d4dfa657f1e4f14433
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2024-05-04 01:52:21 +0200

    chore: update semantic config

--- release.config.cjs
+++ release.config.cjs
@@ -2,14 +2,21 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    '@semantic-release/npm',
     [
-      '@semantic-release/github',
+      '@semantic-release/changelog',
       {
-        assets: 'release/*.tgz',
+        changelogFile: 'CHANGELOG.md',
       },
     ],
-    '@semantic-release/git',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json'],
+        message:
+          'chore(release): set `package.json` to ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
   ],
   branches: ['main'],
   preset: 'angular',
